95e8418ab147cb368e3ed2b2bf2ae4e1d79e508f
RubenF <ruben.flecha@gmail.com>
2018-12-29 19:42:33 -0300

    Update diciembre 2018

---
+++ vast_mc2.R
@@ -0,0 +1,10 @@
+#=========================  TP3 DATAVIZ  ==============================
+rm(list=ls()) 
+# Path Fede
+#setwd("~/Data Mining/03.Visualizacion de Datos/Trabajos Practicos/TP3")
+setwd("/Users/rubenflecha/Documents/DataViz/TPs/TP 3") 
+#----------- PREPARACION DE LOS DATOS --------------------------------
+getwd()
+require(dplyr)
+if(!require("dplyr")) install.packages("dplyr")
+library(dplyr)
